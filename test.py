9c234fb36e0531b13799dacc94bbcace40bdaccd
HandAndy326 <221110@trinityschools.nz>
2024-05-13 14:30:39 +1200

    Working program

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello world")
+import random
+
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
+operator = random.choice(['+', '-', '*'])
+if operator == '+':
+    answer = num1 + num2
+elif operator == '-':
+    answer = num1 - num2
+elif operator == '*':
+    answer = num1 * num2
+user = int(input(f"What is {num1} {operator} {num2} = "))
+if user == answer:
+    print("correct!")
+if user != answer:
+    print("wrong")
