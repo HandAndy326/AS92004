bfa8b7a3f6181aeb048bad7e8544ca3a1742a5a4
HandAndy326 <221110@trinityschools.nz>
2024-05-14 12:21:32 +1200

    flexible code

--- test.py
+++ test.py
@@ -1,14 +1,38 @@
 import random
+instruction = input("Do you know how to do a maths quiz? ").lower()
+instrustions = instruction[0]
+if instrustions == ("n"):
+    print('''You are going to select a difficulty out of primary and intermediate.
+Then you are going to get a question that is randomly generated which you have to answer
+''')
+elif instrustions == ("y"):
+    print("Let's start")
+else:
+    print("Please enter either yes or no")
 
-num1 = random.randint(1, 10)
-num2 = random.randint(1, 10)
-operator = random.choice(['+', '-', '*'])
+difficulty = input("Do you want the primary school questions or the intermediate school questions? ")
+if difficulty == ("primary"):
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    operator = random.choice(['+', '-',])
+elif difficulty == ("intermediate"):
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    operator = random.choice(['+', '-',])
 if operator == '+':
     answer = num1 + num2
 elif operator == '-':
     answer = num1 - num2
+
 user = int(input(f"What is {num1} {operator} {num2} = "))
+
 if user == answer:
     print("correct!")
-if user != answer:
+    
+elif user != answer:
     print("wrong")
+    print("try again")
+
+
+    
+
