0c6a835052dd854e2061bb9d83fad0f8a4725efa
HandAndy326 <221110@trinityschools.nz>
2024-05-15 12:05:49 +1200

    Number of questions and difficulty working

--- test.py
+++ test.py
@@ -9,29 +9,32 @@ elif instrustions == ("y"):
     print("Let's start")
 else:
     print("Please enter either yes or no")
+number_of_questions = 0
+number_of_questions = int(input("How many questions do you want? "))
 
-difficulty = input("Do you want the primary school questions or the intermediate school questions? ")
-if difficulty == ("primary"):
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
-    operator = random.choice(['+', '-',])
-elif difficulty == ("intermediate"):
-    num1 = random.randint(1, 100)
-    num2 = random.randint(1, 100)
-    operator = random.choice(['+', '-',])
-if operator == '+':
-    answer = num1 + num2
-elif operator == '-':
-    answer = num1 - num2
-
-user = int(input(f"What is {num1} {operator} {num2} = "))
-
-if user == answer:
-    print("correct!")
-    
-elif user != answer:
-    print("wrong")
-    print("try again")
+difficulty = input("Do you want the primary school questions or the intermediate school questions? ").lower()
+difficulty2 = difficulty[0]
+for i in range(number_of_questions):
+    operator = ''
+    num1 = 0
+    num2 = 0
+    if difficulty2 == ("p"):
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        operator = random.choice(['+', '-',])
+    elif difficulty2 == ("i"):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        operator = random.choice(['+', '-',])
+    if operator == '+':
+        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
+    user = int(input(f"What is {num1} {operator} {num2} = "))
+    if user == answer:
+        print("correct!")
+    elif user != answer:
+        print("wrong")
 
 
     
